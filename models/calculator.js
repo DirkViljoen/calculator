875e4831a10fa12df823b79756d0552d79e3b727
DirkViljoen <dirkcharl.viljoen@gmail.com>
2016-03-01 14:37:27 +0200

    feat(calculator) Add model

---
+++ models/calculator.js
@@ -0,0 +1,27 @@
+var calculator = function() {
+
+    function add(v1, v2) {
+      return v1 + v2;
+    };
+
+    function subtract(v1, v2) {
+      return v1 - v2;
+    };
+
+    function multiply(v1, v2) {
+      return v1 * v2;
+    };
+
+    function divide(v1, v2) {
+      return v1 / v2;
+    }
+
+    return {
+        add: add,
+        subtract: subtract,
+        multiply: multiply,
+        divide: divide
+    };
+};
+
+module.exports = calculator;
